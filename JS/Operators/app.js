d64a24c428cf2a0d51bbc5a7cb4d747a1ff63eca
princerxj <pr273582@gmail.com>
2024-07-04 22:54:45 +0530

    Operators in JS

---
+++ JS/Operators/app.js
@@ -0,0 +1,23 @@
+// Arithmetic Operators (+,-,*,**,/,%)
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+//Unary Operators (++,--)
+a = 10;
+console.log(a++); //Post Increment
+console.log(++a); //Pre Increment
+
+//Assignment Oeprators (=,+=,*=,/=,%= etc.)
+a = 10;
+b = a;
+console.log(b);
+
+//Comparison Operators (>,>=,<,<=,!=,==, ===) gives true or false
+let age = 18;
+console.log(age>18);
