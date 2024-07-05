cfcf654fb019332982967892cbd94fbe1467667c
princerxj <pr273582@gmail.com>
2024-07-05 20:00:57 +0530

    If Statement

--- JS/Conditional-Statements/app.js
+++ JS/Conditional-Statements/app.js
@@ -3,3 +3,14 @@
 // nested if-else
 // switch
 
+//if statement example
+
+console.log("Before my if statement");
+let age = 13;
+if(age >= 18) {
+    console.log("You can vote");
+}
+if(age < 18) {
+    console.log("You cannot vote");
+}
+console.log("After my if statement");
