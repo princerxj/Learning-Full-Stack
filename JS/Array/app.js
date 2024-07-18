930c54e10802f25685bf890c129c9b481742b839
princerxj <pr273582@gmail.com>
2024-07-19 00:03:48 +0530

    Array Methods

--- JS/Array/app.js
+++ JS/Array/app.js
@@ -1,4 +1,12 @@
-//Arrays are mutable 
+let cars = ["audi", "bmw", "xuv", "maruti"];
+cars.push("toyota"); //Array push method is used to add an element to the end of array 
+console.log(cars);
 
-let fruits = ["mango", "apple", "orange"];
-fruits[0] = "banana"; //fruits = ["banana", "apple", "orange"];
+cars.pop(); //Array pop method Delete the end element and return the array 
+console.log(cars);
+
+cars.unshift("Toyota"); //Array unshift method Add an element to the start of the array
+console.log(cars);
+
+cars.shift(); //Array Shift Method delete an element from the start and returns it 
+console.log(cars);
