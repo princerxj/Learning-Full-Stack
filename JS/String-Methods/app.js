723bbe36a4d75ec7cf18e288369ce0e3a6e21fc7
princerxj <pr273582@gmail.com>
2024-07-18 22:38:07 +0530

    Replace and Repeat Method

--- JS/String-Methods/app.js
+++ JS/String-Methods/app.js
@@ -1,13 +1,11 @@
-//Slice
-//Returns a part of the original string as a new String
-/* Syntax : 
-1.)str.slice(start,end); Ending index is excluded 
-2.)Another method is to just pass the start value , in that 
-case end will be assumed as str.length
-3.)str.slice(-num) will be processed as atr.slice(str.length - num)
+//Replace Method 
+//Searches a value and replaces it with another value 
 
-*/
-let str = "ilovecoding";
-console.log(str.slice(1,5)); //Output : love
-console.log(str.slice(5)); //Output : Coding
-console.log(str.slice(-2)); //Will be processed as 11-2 = 9 i.e str.slice(9) so output will be "ng"
+let str = "ILoveCoding";
+console.log(str.replace("Love","do")); //Output : IdoCoding
+console.log(str); //String will remain same
+
+//Repeat Method
+//repeats the string 
+
+console.log(str.repeat(3)); //Output : ILoveCodingILoveCodingILoveCoding
