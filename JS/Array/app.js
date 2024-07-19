64f9e2824541d504418700247817b6d33e196cd7
princerxj <pr273582@gmail.com>
2024-07-19 20:43:58 +0530

    Slice method in array

--- JS/Array/app.js
+++ JS/Array/app.js
@@ -1,7 +1,10 @@
-//Concat method : merge two arrays 
-let primary = ["red", "yellow", "blue"];
-let secondary = ["orange", "green", "voilet"];
-console.log(primary.concat(secondary));
+//Slice Method in Array : Copies a portion of an array 
+//Behave somewhat like the slice method in string with syntax (start,end)
+let colors = ["red", "yellow", "blue", "orange", "pink", "white"];
 
-//Reverse method : Reverse an array 
-console.log(primary.reverse());
+console.log(colors.slice()); //Output : ['red', 'yellow', 'blue', 'orange', 'pink', 'white']
+console.log(colors.slice(2)); //Output : ['blue', 'orange', 'pink', 'white']
+
+//If passed negative value in the method, this means we want that number of elements from the end of the array 
+console.log(colors.slice(-1)); //Output : ['white']
+console.log(colors.slice(-2)); //Output : ['pink', 'white']
