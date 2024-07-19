fae3dd71ee9a4d28d42ef76a38fa5b9a1cd4bf4a
princerxj <pr273582@gmail.com>
2024-07-19 20:31:38 +0530

    Concat and reverse method

--- JS/Array/app.js
+++ JS/Array/app.js
@@ -1,21 +1,7 @@
-let cars = ["audi", "bmw", "xuv", "maruti"];
-cars.push("toyota"); //Array push method is used to add an element to the end of array 
-console.log(cars);
+//Concat method : merge two arrays 
+let primary = ["red", "yellow", "blue"];
+let secondary = ["orange", "green", "voilet"];
+console.log(primary.concat(secondary));
 
-cars.pop(); //Array pop method Delete the end element and return the array 
-console.log(cars);
-
-cars.unshift("Toyota"); //Array unshift method Add an element to the start of the array
-console.log(cars);
-
-cars.shift(); //Array Shift Method delete an element from the start and returns it 
-console.log(cars);
-
-//IndexOf : Returns the index of something 
-console.log(cars.indexOf("bmw")); //Output : 1
-console.log(cars.indexOf("xuv")); //output : 2
-console.log(cars.indexOf("toyota")); //Output : -1
-
-//includes : Tells the presence of an element in the array in boolean 
-console.log(cars.includes("xuv")); //Output : True 
-console.log(cars.includes("toyota")); //Output : False
+//Reverse method : Reverse an array 
+console.log(primary.reverse());
