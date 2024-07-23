601096789bee2c1f972d431988d66d6a8694c979
princerxj <pr273582@gmail.com>
2024-07-23 15:27:23 +0530

    Splice method in array

--- JS/Array/app.js
+++ JS/Array/app.js
@@ -1,10 +1,12 @@
-//Slice Method in Array : Copies a portion of an array 
-//Behave somewhat like the slice method in string with syntax (start,end)
+//Splice method in Array Methods 
+//Splice : removes/replaces/add elements in place 
+// splice(start,deleteCount,item0........itemN)
+//Change happens in original array 
+
 let colors = ["red", "yellow", "blue", "orange", "pink", "white"];
-
-console.log(colors.slice()); //Output : ['red', 'yellow', 'blue', 'orange', 'pink', 'white']
-console.log(colors.slice(2)); //Output : ['blue', 'orange', 'pink', 'white']
-
-//If passed negative value in the method, this means we want that number of elements from the end of the array 
-console.log(colors.slice(-1)); //Output : ['white']
-console.log(colors.slice(-2)); //Output : ['pink', 'white']
+console.log(colors.splice(4)); //Output : ['pink', 'white'] : elements till index 3 will be removed 
+console.log(colors); //Output : ['red', 'yellow', 'blue', 'orange']
+console.log(colors.splice(0,1)); //Output : ['red']
+console.log(colors); //Output : ['yellow', 'blue', 'orange]
+colors.splice(0,1,"Magenta", "Grey");
+console.log(colors);
