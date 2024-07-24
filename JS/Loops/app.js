fbb16262fcc7cbc027b515d96d9d62588e37eb53
princerxj <pr273582@gmail.com>
2024-07-24 22:54:30 +0530

    For loops

---
+++ JS/Loops/app.js
@@ -0,0 +1,10 @@
+//Loops : Used to iterate a piece of code 
+/* For loops : 
+    syntax : for(initialisation;condition;updation) {
+                //Do something 
+             }
+*/
+
+for(let i = 0; i <= 5; i++) {
+    console.log(i);
+}
