681c1e1ef20c4cdea9aa51308a8be10fe66f7d77
princerxj <pr273582@gmail.com>
2024-10-30 11:04:39 +0530

    Example of object literal

---
+++ JS/Objects/app.js
@@ -0,0 +1,12 @@
+/* Object Literals :
+    Used to store keyed collections and complex entities.
+    property => (key,value) pair
+    Objects are a collection of properties
+    For example : name = Prince , here name is the key and Prince is the value
+*/
+const student = {
+    name : "Shradha",
+    marks : 90,
+    location : "Delhi",
+    age : 23,
+};
