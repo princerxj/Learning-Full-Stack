b6ac5f0cf8078969739346fcde3344c5db2b9fa8
princerxj <pr273582@gmail.com>
2024-10-30 14:36:46 +0530

    Math Objects

---
+++ JS/Objects/Math-Objects/app.js
@@ -0,0 +1,21 @@
+//Math Objects
+
+//Properties : for example : Math.PI, Math.E; etc.
+
+//Methods : Math.abs(n);  Math.pow(a,b); Math.floor(n); Math.ceil(n); Math.random(); etc.
+
+console.log(Math.abs(-2.303));
+console.log(Math.abs(9.3));
+
+console.log(Math.pow(2,4));
+
+console.log(Math.floor(5.4));
+console.log(Math.floor(-3.2));
+
+console.log(Math.ceil(9.202));
+console.log(Math.ceil(-2.3));
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
