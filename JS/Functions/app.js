7608b3e78afb38b596bd21549f75d16fb3d21775
princerxj <pr273582@gmail.com>
2024-11-01 16:18:26 +0530

    functions

---
+++ JS/Functions/app.js
@@ -0,0 +1,12 @@
+//Syntax : 
+/* 
+    function definition : 
+    function funcName() {
+        //do something
+    }
+    Function calling : 
+    funcname();
+*/
+function hello() {
+    console.log("Hello");
+}
