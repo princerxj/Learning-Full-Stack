f1f83844b631e57c86d2837a82337ecbf4ad128e
princerxj <pr273582@gmail.com>
2024-11-01 16:29:13 +0530

    Examples

--- JS/Functions/app.js
+++ JS/Functions/app.js
@@ -10,3 +10,22 @@
 function hello() {
     console.log("Hello");
 }
+
+function greet() {
+    let name = prompt('Enter your name  : ');
+    console.log(`Hello ${name}`);
+}
+
+function printTable() {
+    let n = prompt("Enter a number to print table : ");
+    for(let i = 1; i < 11; i++) {
+        console.log(`${n} X ${i} = ${n*i}`);
+    }
+}
+
+function rolldice() {
+    let num = Math.random();
+    num *= 6;
+    num = Math.floor(num) + 1;
+    console.log(num);
+}
